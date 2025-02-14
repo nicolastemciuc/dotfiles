3efb16043d836405f629f3cbb2161c4db5bad979
Nicolas Temciuc <nicotemciuc@gmail.com>
2025-02-14 14:51:05 -0300

    chore: restore lspconfig

---
+++ nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    opts = {
+      auto_install = true,
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+      local lspconfig = require("lspconfig")
+      lspconfig.ruby_lsp.setup({
+        capabilities = capabilities
+      })
+    end,
+  },
+}
