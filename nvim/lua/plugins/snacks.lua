48a8c6d67d579d628c1438ee8218ed8223ea46e2
Nicolas Temciuc <nicotemciuc@gmail.com>
2025-02-14 14:44:18 -0300

    chore: use snack for lsp config

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -25,7 +25,16 @@ return {
     indent = { enabled = true },
     input = { enabled = true },
     git = { enabled = true },
-    picker = { enabled = true },
+    picker = {
+      enabled = true,
+      win = {
+        input = {
+          keys = {
+           ["<Esc>"] = { "close", mode = { "n", "i" } },
+          }
+        }
+      },
+    },
     notifier = { enabled = true },
     quickfile = { enabled = true },
     scroll = { enabled = false },
@@ -35,10 +44,28 @@ return {
   keys = {
     { "<leader>sf",       function() Snacks.scratch() end,            desc = "Toggle Scratch Buffer" },
     { "<leader>S",        function() Snacks.scratch.select() end,     desc = "Select Scratch Buffer" },
-    { "<C-p>",            function() Snacks.picker.pick("files") end, desc = "Find Files" },
+    { "<C-p>",
+      function()
+        Snacks.picker.files({
+          hidden = true,
+          ignored = true,
+          exclude = { ".git/", ".ruby-lsp/", "tmp/", "coverage/", "log/" }
+        })
+      end,
+      desc = "Find Files"
+    },
     { "<leader><leader>", function() Snacks.picker.recent() end,      desc = "Recent Files" },
     { "<leader>fb",       function() Snacks.picker.buffers() end,     desc = "Buffers" },
-    { "<leader>fg",       function() Snacks.picker.grep() end,        desc = "Grep Files" },
+    { "<leader>fg",
+      function()
+        Snacks.picker.grep({
+          hidden = true,
+          ignored = true,
+          exclude = { ".git/", ".ruby-lsp/", "tmp/", "coverage/", "log/" }
+        })
+      end,
+      desc = "Grep Files"
+    },
     { "<C-n>",            function() Snacks.explorer() end,           desc = "Explorer" },
     -- git
     { "<leader>gb", function() Snacks.picker.git_branches() end, desc = "Git Branches" },
@@ -48,5 +75,13 @@ return {
     { "<leader>gS", function() Snacks.picker.git_stash() end, desc = "Git Stash" },
     { "<leader>gd", function() Snacks.picker.git_diff() end, desc = "Git Diff (Hunks)" },
     { "<leader>gf", function() Snacks.picker.git_log_file() end, desc = "Git Log File" },
+    -- LSP
+    { "gd", function() Snacks.picker.lsp_definitions() end, desc = "Goto Definition" },
+    { "gD", function() Snacks.picker.lsp_declarations() end, desc = "Goto Declaration" },
+    { "gr", function() Snacks.picker.lsp_references() end, nowait = true, desc = "References" },
+    { "gI", function() Snacks.picker.lsp_implementations() end, desc = "Goto Implementation" },
+    { "gy", function() Snacks.picker.lsp_type_definitions() end, desc = "Goto T[y]pe Definition" },
+    { "<leader>ss", function() Snacks.picker.lsp_symbols() end, desc = "LSP Symbols" },
+    { "<leader>sS", function() Snacks.picker.lsp_workspace_symbols() end, desc = "LSP Workspace Symbols" },
   }
 }
