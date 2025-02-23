4d1b6471fba6e7e4ff32b318f008a748342ee0f9
Nicolas Temciuc <nicotemciuc@gmail.com>
2025-02-23 09:29:42 -0300

    chore: change current directory
    Previously when we execute `nvim ~/some/path`, neovim used the terminal
    directory with `find files`, `fuzzy finding` or the tree. Now it uses
    the path that we pass
    
    Also when we open a gem using `gf` in the `Gemfile.lock` or `gem open
    rails` the directory change to the gem directory

--- nvim/init.lua
+++ nvim/init.lua
@@ -29,3 +29,35 @@ local ghe_url = os.getenv("GITHUB_ENTERPRISE_URL")
 if ghe_url then
   vim.g.github_enterprise_urls = { ghe_url }
 end
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    local arg = vim.fn.argv(0)
+    if arg and vim.fn.isdirectory(arg) == 1 then
+      -- If a directory is opened, set it as the working directory
+      vim.cmd('cd ' .. arg)
+    elseif arg and vim.fn.filereadable(arg) == 1 then
+      -- If a file is opened, set the working directory to its parent directory
+      vim.cmd('cd ' .. vim.fn.fnamemodify(arg, ':h'))
+    end
+  end,
+})
+
+local app_root = nil
+
+vim.api.nvim_create_autocmd("BufEnter", {
+  callback = function()
+    local path = vim.fn.expand("%:p")
+    if path:match("/gems/") then
+      -- Save the app's root if not already saved
+      if not app_root then
+        app_root = vim.fn.getcwd()
+      end
+      -- Change directory to the gem's location
+      vim.cmd("cd " .. vim.fn.fnamemodify(path, ":h"))
+    elseif app_root then
+      -- If leaving the gem and the app's root is saved, restore it
+      vim.cmd("cd " .. app_root)
+    end
+  end,
+})
