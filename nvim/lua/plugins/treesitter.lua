231a01e9d51e1c2c5d818ee3011605203cdcc68f
Nicolas Temciuc <nicotemciuc@gmail.com>
2025-02-23 10:04:38 -0300

    chore(treesitter): only disable highlight for .html.erb

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -6,7 +6,18 @@ return {
       local config = require("nvim-treesitter.configs")
       config.setup({
         auto_install = true,
-        highlight = { enable = true },
+        highlight = {
+          enable = true,
+          -- Disable highlighting for specific file types using the 'disable' option
+          disable = function(lang, bufnr)
+            local filename = vim.api.nvim_buf_get_name(bufnr)
+            -- Disable for .html.erb files
+            if filename:match("%.html.erb$") then
+              return true
+            end
+            return false
+          end,
+        },
         indent = { enable = true },
       })
     end
